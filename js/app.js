32a21c01ec4991b75131719d5bc144ad5dc0cd26
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-05-05 15:28:35 -0700

    Make table with data and header and footer

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
 'use strict';
 var hours = ['6:00am','7:00am','8:00am', '9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm'];
 
+function createTableHeader() {
+  // make thead
+  var parent = document.getElementById('cookieChart');
+  var thead = document.createElement('thead');
+  parent.appendChild(thead);
+  // make tr
+  var tr = document.createElement('tr');
+  thead.appendChild(tr);
+  // make first space empty
+  var timeHeader = document.createElement('th');
+  timeHeader.textContent = '';
+  tr.appendChild(timeHeader);
+  // put times in
+  for (var i = 0; i < hours.length; i++){
+    timeHeader = document.createElement('th');
+    timeHeader.textContent = hours[i];
+    tr.appendChild(timeHeader);
+  }
+  // put Total in
+  timeHeader = document.createElement('th');
+  timeHeader.textContent = 'Total';
+  tr.appendChild(timeHeader);
+}
+
 function CookiesPerLocation(location, minimumCustomers, maximumCustomers, avgCookieSale) {
   this.location = location;
   this.minimumCustomers = minimumCustomers;
@@ -28,15 +52,50 @@ CookiesPerLocation.prototype.makeCookieArray = function(){
   this.cookieArray.push(this.total);
 };
 
-CookiesPerLocation.prototype.addListToPage = function(){
+CookiesPerLocation.prototype.render = function(){
   this.makeCookieArray();
+  var parent = document.getElementById('cookieChart');
+  var locationRow = document.createElement('tr');
+  parent.appendChild(locationRow);
+  var rowTitle = document.createElement('th');
+  rowTitle.textContent = this.location;
+  locationRow.appendChild(rowTitle);
   for (var j = 0; j < this.cookieArray.length; j++) {
-    var parent = document.getElementById(this.location.toLowerCase());
-    var listItem = document.createElement('li');
-    listItem.textContent = this.cookieArray[j];
-    parent.appendChild(listItem);
+    var cookieData = document.createElement('td');
+    cookieData.textContent = this.cookieArray[j];
+    locationRow.appendChild(cookieData);
   }
 };
+
+function getTotals(){
+  var totals = [];
+  var hourlyTotal = 0;
+  for (var i = 0; i < (hours.length + 1); i++) {
+    hourlyTotal = seattle.cookieArray[i] + tokyo.cookieArray[i] + dubai.cookieArray[i] + paris.cookieArray[i] + lima.cookieArray[i];
+    totals.push(hourlyTotal);
+  }
+  return totals;
+}
+
+function renderHourlyTotals(){
+  // make tfoot
+  var parent = document.getElementById('cookieChart');
+  var tfoot = document.createElement('tfoot');
+  parent.appendChild(tfoot);
+  // make row
+  var tr = document.createElement('tr');
+  tfoot.appendChild(tr);
+  // make first line
+  var totalsTitle = document.createElement('th');
+  totalsTitle.textContent = 'Totals:';
+  tr.appendChild(totalsTitle);
+  // add data
+  for (var i = 0; i < getTotals().length; i++){
+    var hourTotal = document.createElement('td');
+    hourTotal.textContent = getTotals()[i];
+    tr.appendChild(hourTotal);
+  }
+}
 
 var seattle = new CookiesPerLocation('Seattle',23,65,6.3);
 var tokyo = new CookiesPerLocation('Tokyo',3,24,1.2);
@@ -44,9 +103,13 @@ var dubai = new CookiesPerLocation('Dubai',11,38,3.7);
 var paris = new CookiesPerLocation('Paris', 20, 38, 2.3);
 var lima = new CookiesPerLocation('Lima',2,16,4.6);
 
-seattle.addListToPage();
-tokyo.addListToPage();
-dubai.addListToPage();
-paris.addListToPage();
-lima.addListToPage();
+
+createTableHeader();
+seattle.render();
+tokyo.render();
+dubai.render();
+paris.render();
+lima.render();
+getTotals();
+renderHourlyTotals();
 
